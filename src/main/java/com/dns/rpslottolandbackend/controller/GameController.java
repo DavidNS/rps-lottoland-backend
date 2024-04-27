f69d4dade9dbf356a88afa0c08c84004c4211b38
bender <davidnowakowski92@gmail.com>
2024-04-27 18:19:48 +0200

    feat(Game): Player1 random RPS + Game resolver

--- src/main/java/com/dns/rpslottolandbackend/controller/GameController.java
+++ src/main/java/com/dns/rpslottolandbackend/controller/GameController.java
@@ -3,12 +3,18 @@ package com.dns.rpslottolandbackend.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dns.rpslottolandbackend.dto.GameOut;
+import com.dns.rpslottolandbackend.service.GameService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
+@AllArgsConstructor
 public class GameController {
 
+	private final GameService gameService;
+
 	public GameOut newGame(String id) {
-		return new GameOut();
+		return gameService.newGame();
 	}
 
 }
--- src/test/java/com/dns/rpslottolandbackend/controller/GameControllerTest.java
+++ src/test/java/com/dns/rpslottolandbackend/controller/GameControllerTest.java
@@ -1,8 +1,6 @@
 package com.dns.rpslottolandbackend.controller;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -18,21 +16,24 @@ import com.dns.rpslottolandbackend.dto.RockPaperScissors;
 import com.dns.rpslottolandbackend.service.GameService;
 
 class GameControllerTest {
-	
+
 	private GameController testInstance;
-	
+
+	private GameService gameService;
+
 	@BeforeEach
-	public void createInstance(){
-		testInstance = new GameController();
+	public void createInstance() {
+		gameService = new GameService();
+		testInstance = new GameController(gameService);
 	}
-	
+
 	@Test
 	void testAllInputOkReturnsNotNull() {
 		String id = "1";
 		GameOut result = testInstance.newGame(id);
 		assertNotNull(result);
 	}
-	
+
 	@Test
 	void testAllInputOkPlayer2IsAlwaysRock() {
 		String id = "1";
@@ -43,6 +44,52 @@ class GameControllerTest {
 			assertEquals(expectedPlayer2, result.getPlayer2Result());
 		}
 	}
-	
-	
+
+	@Test
+	void testAllInputOkPlayer1IsRockPaperOrScissors() {
+		String id = "1";
+		int retries = 100;
+		List<RockPaperScissors> possibleResults = Arrays.asList(RockPaperScissors.values());
+		for (int i = 0; i < retries; i++) {
+			GameOut result = testInstance.newGame(id);
+			assertTrue(possibleResults.contains(result.getPlayer1Result()));
+		}
+	}
+
+	@Test
+	void testPlayer1WinsIfPaper() {
+		String id = "1";
+		RockPaperScissors resultPlayer1Condition = RockPaperScissors.PAPER;
+		GameResult expected = GameResult.WIN_PLAYER_1;
+		GameOut result = testInstance.newGame(id);
+		while (!resultPlayer1Condition.equals(result.getPlayer1Result())) {
+			result = testInstance.newGame(id);
+		}
+		assertEquals(expected, result.getGameResult());
+	}
+
+	@Test
+	void testPlayer1LossIfScissors() {
+		String id = "1";
+		RockPaperScissors resultPlayer1Condition = RockPaperScissors.SCISSORS;
+		GameResult expected = GameResult.WIN_PLAYER_2;
+		GameOut result = testInstance.newGame(id);
+		while (!resultPlayer1Condition.equals(result.getPlayer1Result())) {
+			result = testInstance.newGame(id);
+		}
+		assertEquals(expected, result.getGameResult());
+	}
+
+	@Test
+	void testPlayer1DrawIfRock() {
+		String id = "1";
+		RockPaperScissors resultPlayer1Condition = RockPaperScissors.ROCK;
+		GameResult expected = GameResult.DRAW;
+		GameOut result = testInstance.newGame(id);
+		while (!resultPlayer1Condition.equals(result.getPlayer1Result())) {
+			result = testInstance.newGame(id);
+		}
+		assertEquals(expected, result.getGameResult());
+	}
+
 }
