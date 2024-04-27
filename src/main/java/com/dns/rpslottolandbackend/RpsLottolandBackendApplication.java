ab382c5bcd743b2ed725f9162ce95fd1f7019c4e
bender <davidnowakowski92@gmail.com>
2024-04-27 18:01:54 +0200

    feat: Initial commit

---
+++ src/main/java/com/dns/rpslottolandbackend/RpsLottolandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dns.rpslottolandbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsLottolandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpsLottolandBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dns/rpslottolandbackend/RpsLottolandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dns.rpslottolandbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsLottolandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
